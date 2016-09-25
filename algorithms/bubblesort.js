4de3a03f773fb06c41dc170810f6f87ebe5509ea
zhangzju <zhangway19921221@163.com>
2016-09-25 10:51:58 +0800

    add insertSort

---
+++ algorithms/bubblesort.js
@@ -0,0 +1,41 @@
+
+var bubbleSort = (function(){
+  var bSort = function(arr=[]) {
+    let len = arr.length-1;
+    while (len > 0) {
+      let pos = 0; //已经排序好的最后一个位置
+      for (var i = 0; i < len; i++) {
+        if (arr[i] > arr[i+1]) {
+          pos = i;
+          [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+        }
+      }
+      i = pos;
+    }
+    return arr;
+  };
+
+  var bSort_double = function(arr=[]) {
+    var low = 0;
+    var high= arr.length-1; //设置变量的初始值
+    var tmp,j;
+    while (low < high) {
+        for (j= low; j< high; ++j) //正向冒泡,找到最大者
+            if (arr[j]> arr[j+1]) {
+                tmp = arr[j]; arr[j]=arr[j+1];arr[j+1]=tmp;
+            }
+        --high;                 //修改high值, 前移一位
+        for (j=high; j>low; --j) //反向冒泡,找到最小者
+            if (arr[j]<arr[j-1]) {
+                tmp = arr[j]; arr[j]=arr[j-1];arr[j-1]=tmp;
+            }
+        ++low;                  //修改low值,后移一位
+    }
+    return arr3;
+  };
+
+  return {
+    bSort: bSourt,
+    bSort_double: bSort_double
+  }
+})();
--- datastructure/doublelinkedlist.js
+++ datastructure/doublelinkedlist.js
@@ -1,16 +1,15 @@
-
 class Node{
   constructor(value){
     this.value = value;
     this.next = null;
+    this.prev = null;
   }
 }
 
-export default class sList{
+export default class dList{
   constructor(){
     this.head = new Node("head");
     this.find = find;
-    this.findPrev = fidnPrev;
     this.insert = insert;
     this.remove = remove;
   }
@@ -23,28 +22,19 @@ export default class sList{
     return current;
   }
 
-  findPrev(value){
-    var current = this.head;
-    var next = current.next;
-    if (next.value == value) {
-      return current;
-    }else {
-      current = next;
-    }
-    return {};
-  }
-
   insert(value, next){
     var newNode = new Node(value);
     var current = this.find(next);
+    newNode.prev = current;
     newNode.next = current.next;
     current.next = newNode;
+    return newNode;
   }
 
   remove(value){
     var current = this.find(value);
-    var prev = this.findPrev(value);
-    prev.next = current.next;
+    current.next.prev = current.prev;
+    current.prev.next = current.next;
     return 0;
   }
 }
