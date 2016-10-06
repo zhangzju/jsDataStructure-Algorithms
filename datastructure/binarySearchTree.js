8637ec678b9db535cb989678fb7f774fa93a1a1e
zhangzju <zhangway19921221@163.com>
2016-10-06 22:45:27 +0800

    change bstree

--- datastructure/binarySearchTree.js
+++ datastructure/binarySearchTree.js
@@ -6,14 +6,31 @@ class Node {
 		this.right = null;
 	}
 }
-
+//节点大于左子树任意节点，小于右子树任意节点
 export default class bstree {
 	constructor(value){
-		this.root = new Node(value);
+		this.node = new Node(value);//make the root
 		this.findMin = findMin;
 		this.findMax = findMax;
 		this.insert = insert;
 		this.contains = contains;
 		this.remove = remove;
+		this.compare = compare;
+	}
+
+	compare(value, storeValue) {
+		if (value > storeValue) {
+			return 1;
+		} else if (value < storeValue) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+
+	findMin(value) {
+		switch(compare(value, this.node.value)){
+			
+		}
 	}
 }
